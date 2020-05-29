71f1e4d37f8ac8af2f7a3a57366cd443ff8a74b0
Binu42 <kbinu42@gmail.com>
2020-05-29 17:22:13 +0530

    new build

---
+++ precache-manifest.ba2b5ce6c09da47bac6f8339ffabbc38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9322cdb1f478455840f90ce344fe1f",
+    "url": "/webDevsCom/index.html"
+  },
+  {
+    "revision": "00c40fb08a7c53ce7530",
+    "url": "/webDevsCom/static/css/2.ebd2bdea.chunk.css"
+  },
+  {
+    "revision": "dd6df67abfbd3fa8cb2d",
+    "url": "/webDevsCom/static/css/main.68b37e60.chunk.css"
+  },
+  {
+    "revision": "00c40fb08a7c53ce7530",
+    "url": "/webDevsCom/static/js/2.e2d959fa.chunk.js"
+  },
+  {
+    "revision": "7c8b7808c34cb59049374f5d75e06b5f",
+    "url": "/webDevsCom/static/js/2.e2d959fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6df67abfbd3fa8cb2d",
+    "url": "/webDevsCom/static/js/main.6227c276.chunk.js"
+  },
+  {
+    "revision": "f967d822a7a3e7793929",
+    "url": "/webDevsCom/static/js/runtime-main.5b24c7f9.js"
+  }
+]);
