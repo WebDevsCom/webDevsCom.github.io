2102ab4674167d5726eb34eed3507a91ab2183ef
Binu42 <kbinu42@gmail.com>
2020-06-06 01:06:31 +0530

    new build

---
+++ precache-manifest.e5e0a3e2ce953a4d7f2f5193a4ab7729.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69273cdb6f082996e8717f42349b95c",
+    "url": "/webDevsCom/index.html"
+  },
+  {
+    "revision": "603218c7dc5e8d2e5fb6",
+    "url": "/webDevsCom/static/css/2.5592ad1b.chunk.css"
+  },
+  {
+    "revision": "c1ec7b29e3f8db491eb5",
+    "url": "/webDevsCom/static/css/3.a2c18f5e.chunk.css"
+  },
+  {
+    "revision": "060e6dca1899f1965b81",
+    "url": "/webDevsCom/static/css/main.2666d455.chunk.css"
+  },
+  {
+    "revision": "603218c7dc5e8d2e5fb6",
+    "url": "/webDevsCom/static/js/2.66786df1.chunk.js"
+  },
+  {
+    "revision": "54d2c0b971a350b90a37f9ebef9787e8",
+    "url": "/webDevsCom/static/js/2.66786df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ec7b29e3f8db491eb5",
+    "url": "/webDevsCom/static/js/3.2da8e2b8.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/webDevsCom/static/js/3.2da8e2b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05227b96486e8d9ffd3d",
+    "url": "/webDevsCom/static/js/4.d536d88a.chunk.js"
+  },
+  {
+    "revision": "b3df7557b0317be642b659930d0c7326",
+    "url": "/webDevsCom/static/js/4.d536d88a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30850791c3e5149bd71b",
+    "url": "/webDevsCom/static/js/5.9dd51d1f.chunk.js"
+  },
+  {
+    "revision": "698e86a332fc18879d1b",
+    "url": "/webDevsCom/static/js/6.566f55d1.chunk.js"
+  },
+  {
+    "revision": "4d5698d70f910a0ac3ce",
+    "url": "/webDevsCom/static/js/7.992537f1.chunk.js"
+  },
+  {
+    "revision": "060e6dca1899f1965b81",
+    "url": "/webDevsCom/static/js/main.f2891867.chunk.js"
+  },
+  {
+    "revision": "f6c87ea51776540f302e",
+    "url": "/webDevsCom/static/js/runtime-main.3af85398.js"
+  }
+]);
