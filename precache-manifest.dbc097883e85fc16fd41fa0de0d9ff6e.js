1dd54b21d1a824f6f5920e5d17de9be6096f4a6d
Binu42 <kbinu42@gmail.com>
2020-10-23 11:02:49 +0530

    new build

---
+++ precache-manifest.dbc097883e85fc16fd41fa0de0d9ff6e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259643de0bc526e51ae8bd192bd5094f",
+    "url": "/webDevsCom/index.html"
+  },
+  {
+    "revision": "1893a247dd90b69c0069",
+    "url": "/webDevsCom/static/css/2.ebd2bdea.chunk.css"
+  },
+  {
+    "revision": "8256c3c9f083d086959f",
+    "url": "/webDevsCom/static/css/main.95c9a713.chunk.css"
+  },
+  {
+    "revision": "1893a247dd90b69c0069",
+    "url": "/webDevsCom/static/js/2.2771f2ee.chunk.js"
+  },
+  {
+    "revision": "0a8f352bea3bf0f1cd79987914b4f867",
+    "url": "/webDevsCom/static/js/2.2771f2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ee6d93e93d45d546fc",
+    "url": "/webDevsCom/static/js/3.080381a5.chunk.js"
+  },
+  {
+    "revision": "b3df7557b0317be642b659930d0c7326",
+    "url": "/webDevsCom/static/js/3.080381a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81418175ae38a8b24cd",
+    "url": "/webDevsCom/static/js/4.c1e2297c.chunk.js"
+  },
+  {
+    "revision": "4f5e534c8091fafd0c83",
+    "url": "/webDevsCom/static/js/5.1ac7ff07.chunk.js"
+  },
+  {
+    "revision": "8256c3c9f083d086959f",
+    "url": "/webDevsCom/static/js/main.cb33271b.chunk.js"
+  },
+  {
+    "revision": "43a0444694cef9584ce3",
+    "url": "/webDevsCom/static/js/runtime-main.796b165c.js"
+  }
+]);
