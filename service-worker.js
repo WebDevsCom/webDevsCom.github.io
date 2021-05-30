5f5ac285ce354d8a811189383a40ea94c262c3df
Binu42 <45959932+Binu42@users.noreply.github.com>
2021-05-30 13:01:44 +0000

    Update master to output generated at 1187ee0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3664e9965fe3d0675cf90d518195f150.js"
+  "/precache-manifest.6592f5e99ef39ff7c8a8e40866877ab5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
